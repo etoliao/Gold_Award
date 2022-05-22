ae79cc8e8e56f146c195d56d855aaa4b5a3aef3a
etoliao <erika.toliao.31@gmail.com>
2022-05-22 14:02:31 -0400

    adding all this fun stuff in

--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.shortcuts import redirect
+from .models import Post
+
 
 def homepage (request):
     return render(request, 'homepage/homepage.html', {}) 
 
+
 def post_list(request):
-    return render(request, 'homepage/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
