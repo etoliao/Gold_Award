848019a41196dcc70a1d81d42269882808480cf6
etoliao <erika.toliao.31@gmail.com>
2022-06-09 14:43:41 -0400

    finally have all of my articles up and site is working

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
-    path('', views.post_list, name='post_list'),
+    path('', views.post_list, name='homepage'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.utils import timezone
-from django.shortcuts import redirect
 from .models import Post
-
-
-def homepage (request):
-    return render(request, 'homepage/homepage.html', {}) 
-
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'homepage/post_list.html', {'posts': posts})
